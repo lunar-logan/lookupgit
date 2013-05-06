3375e05abef79d0363d2b923e8e1f2b079bc7df2
lunar-logan <developerx@live.com>
2013-05-06 15:06:38 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lookupgit::Application.config.secret_token = 'e793970207f77cfcf04fc4dbef85e0597fe11ff2112c77310b8dfba80822c35e575e3be2fe144129b273bc376ede0f44d70e05b29bc8ac3478352416f97fd581'
