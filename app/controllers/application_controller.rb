a5047a43b674384bbb2083b00dc7db7204dba09f
lunar-logan <developerx@live.com>
2013-05-10 16:50:33 +0530

    Loads of changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,19 +10,83 @@ class ApplicationController < ActionController::Base
 
   def lookup
     keyword = params[:keyword]
-    @search = Search.find_by_key(keyword)
-    if @search.nil?
-      #uri = URI('https://github.com/legacy/user/search/:'+keyword)
-      data = open('https://api.github.com/legacy/user/search/:'+keyword, "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
-      @usr = Search.new({key: keyword, info: data, content: nil})
-      @usr.save
+    ##Look if this query is for user by username or email
+    key = nil
+    val = keyword
+    data = {}
+    error = nil
+    @user = nil
+
+    if keyword.include? ':'
+      colon = keyword.index(':')
+      key = keyword[0..colon].strip
+      val = keyword[colon+1..-1].strip
     else
-      data = @search.info
+      val = keyword
     end
-    #raise JSON.load @search
+    #raise key + val + "Values "
+
+    if !key.nil? && key == 'email'
+      #Search using email
+      @user = User.find_by_email(val)
+      if @user.nil? or SearchCache.find_by_key(val).nil?
+        data = JSON.load open('https://api.github.com/legacy/user/email/'+val, "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
+        if data.key?("message")
+          #Something went wrong
+          error = data["message"]
+        else
+          @user = User.new({:name => data["name"],
+                            :username => data["login"],
+                            email: data["email"],
+                            :company => data["company"],
+                            :followers => data["followers_count"],
+                            :following => data["following_count"],
+                            :git_id => data["id"],
+                            :gravatar_id => data["gravatar_id"],
+                            :location => data["location"],
+                            :public_repos => data["public_repo_count"],
+                            :public_gists => data["public_gist_count"],
+                            :url => data["html_url"]
+                           })
+          @user.save
+          data["users"] = @user
+          searchRes = SearchCache.new ({key: val, response: nil})
+          searchRes.save
+        end
+      else
+        data["users"] = @user
+      end
+    else
+      #search using the username
+      @user = User.where('name LIKE ?', "%#{val}%")
+      data["users"] = @user
+      if @user.blank? or SearchCache.find_by_key(val).nil?
+        users = JSON.load open('https://api.github.com/legacy/user/search/:'+val, "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
+        data = users
+        users["users"].each do |usr|
+          @user = User.new({:name => usr["name"],
+                            :username => usr["login"],
+                            email: usr["email"],
+                            :company => usr["company"],
+                            :followers => usr["followers_count"],
+                            :following => usr["following_count"],
+                            :git_id => usr["id"],
+                            :gravatar_id => usr["gravatar_id"],
+                            :location => usr["location"],
+                            :public_repos => usr["public_repo_count"],
+                            :public_gists => usr["public_gist_count"],
+                            :url => usr["html_url"]
+                           })
+          @user.save
+        end
+        searchRes = SearchCache.new ({key: val, response: nil})
+        searchRes.save
+      end
+    end
     respond_to do |format|
       format.json { render json: data }
     end
+
   end
 
   def details
@@ -33,18 +97,33 @@ class ApplicationController < ActionController::Base
     if (uname == nil)
       data = "I'm so sick..." #Just a random response on no querystring, rather it should be empty...
     else
-      @usr = Search.find_by_key(uname)
-      if !@usr.nil? and !@usr.content.nil?
-        data = JSON.load @usr.content
+      searchResponse = Search.find_by_key(uname)
+      if !searchResponse.nil?
+        data = searchResponse.content
       else
-        if @usr.nil?
-          data = JSON.load open('https://api.github.com/users/' + uname, "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
+        @usr = User.find_by_username(uname)
+        if !@usr.nil?
+          data = @usr
         else
-          data = JSON.load @usr.info
+          data = JSON.load open('https://api.github.com/users/' + uname, "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
+          @user = User.new({:name => data["name"],
+                            :username => data["login"],
+                            email: data["email"],
+                            :company => data["company"],
+                            :followers => data["followers_count"],
+                            :following => data["following_count"],
+                            :git_id => data["id"],
+                            :gravatar_id => data["gravatar_id"],
+                            :location => data["location"],
+                            :public_repos => data["public_repo_count"],
+                            :public_gists => data["public_gist_count"],
+                            :url => data["html_url"]
+                           })
+          @user.save
+          data = @user
         end
-
-        user[:username] = data["login"]
-        user[:id] = data["id"]
+        user[:username] = uname #data["login"]
+        user[:id] = data["git_id"]
         user[:gravatar_id] = data["gravatar_id"]
         user[:name] = data["name"]
         user[:company] = data["company"]
@@ -52,11 +131,12 @@ class ApplicationController < ActionController::Base
         user[:blog] = data["blog"]
         user[:public_repos] = data["public_repos"]
         user[:followers] = data["followers"]
+                                ##Done with the owner info
 
-        #Reading the repos data from github, assuming the username is valid
+                                #Reading the repos data from github, assuming the username is valid
         data = open('https://api.github.com/users/' + uname + '/repos', "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
         parsedData = JSON.load(data)
-        #debugData = []
+                                #debugData = []
         parsedData.each do |repo|
           #forks count
           score = repo["forks_count"].to_i * 16 + repo['watchers_count'].to_i * 16 + repo['open_issue'].to_i * 8
@@ -65,8 +145,6 @@ class ApplicationController < ActionController::Base
           collab_url = repo["collaborators_url"]
           contrib_url = repo["contributors_url"]
           collab_url = collab_url[0..-16]
-          #contrib_url = contrib_url[0..-16]
-
           begin
             dat = JSON.load open(collab_url, "Authorization" => "token ef0855cbe7f12bf7b4462b89cc57ce1f2d9f5beb").read
             collab_data = dat.length
@@ -90,8 +168,7 @@ class ApplicationController < ActionController::Base
           repo[:score]=score
           #debugData << {:id => repo["id"], :score => score}
         end
-        #data= debugData.sort_by { |entry| -entry[:score] }
-        #data = debugData
+
         topRepos = []
         repoCount = 0
         threshHold = 3
@@ -118,13 +195,8 @@ class ApplicationController < ActionController::Base
         profile[:user] = user
         profile[:github_repos] = topRepos
         data = profile
-      end
-      if !@usr.nil?
-        @usr.update_attributes({content: data.to_json})
-        @usr.save
-      else
-        @usr = Search.new({key: uname, info: user.to_json, content: data.to_json})
-        @usr.save
+        searchResponse = Search.new({key: uname, info: nil, content: data.to_json})
+        searchResponse.save
       end
     end
     respond_to do |format|
